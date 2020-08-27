9bfa1c47d59c86c225f4a5aa217b732a1f5c89c7
BrunoMSPais <bpais@outlook.pt>
2020-08-27 22:37:51 +0100

    no submit form

--- script.js
+++ script.js
@@ -37,3 +37,81 @@ function setTheme(mode) {
 
     localStorage.setItem('theme', mode);
 }
+
+
+$(document).ready(function() {
+    $('.submit').click(function(e) {
+        e.preventDefault()
+
+        console.log('clicked btn')
+
+        let email = $('#email').val()
+        let subject = $('#subject').val()
+        let name = $('#name').val()
+        let msg = $('#msg').val()
+        let statusElm = $('.status')
+        statusElm.empty()
+
+        function isEmail() {
+
+            if(email.length > 5 && email.includes('@') && email.includes('.')) {
+                statusElm.append('<div>Email is valid</div>')
+                return true
+            } else {
+                e.preventDefault()
+                statusElm.append('<div>Email is not valid</div>')
+                return false
+            }
+        }
+
+        function isSubject() {
+
+            if(subject.length > 2) {
+                statusElm.append('<div>Subject is valid</div>')
+                return true
+            } else {
+                e.preventDefault()
+                statusElm.append('<div>Subject is not valid</div>')
+                return false
+            }
+        }
+
+        function isMsg() {
+            
+            if(msg.length >= 10) {
+                statusElm.append('<div>Message is valid</div>')
+                return true
+            } else {
+                e.preventDefault()
+                statusElm.append('<div>Message is not valid</div>')
+                return false
+            }
+        }
+
+        function isName() {
+
+            if(name.length >= 2) {
+                statusElm.append('<div>Name is valid</div>')
+                return true
+            } else {
+                e.preventDefault()
+                statusElm.append('<div>Name is not valid</div>')
+                return false
+            }
+        }
+
+        function contactSent() {
+            if(!isEmail() || !isMsg() || !isName() || !isSubject()) {
+                document.getElementById('alert').innerHTML = '<p style="color: red;">Email not sent!</p>'
+                console.log('failed')
+            } else {
+                document.getElementById('alert').innerHTML = '<p style="color: green;">Email sent!</p>'
+                console.log('success')
+            }
+        }
+
+        document.getElementById('email-submit-btn').addEventListener('click', () => {
+            return contactSent()
+        })
+    })
+})
